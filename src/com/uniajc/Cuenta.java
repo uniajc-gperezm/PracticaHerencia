1099e878f4462e9c132b58b8920f2927eff076a6
Gabriel Perez <gperezm@profesores.uniajc.edu.co>
2025-03-10 20:57:53 -0500

    Se creo la super clase Cuenta y sus atributos.

---
+++ src/com/uniajc/Cuenta.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.uniajc;
+
+/**
+ *
+ * @author g.perezmoreno
+ */
+public class Cuenta {
+    
+    protected float saldo;
+    protected int numeroConsignaciones = 0;
+    protected int numeroRetiros = 0;
+    protected float tasaAnual;
+    protected float comisionMensual;
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+
+    public int getNumeroConsignaciones() {
+        return numeroConsignaciones;
+    }
+
+    public void setNumeroConsignaciones(int numeroConsignaciones) {
+        this.numeroConsignaciones = numeroConsignaciones;
+    }
+
+    public int getNumeroRetiros() {
+        return numeroRetiros;
+    }
+
+    public void setNumeroRetiros(int numeroRetiros) {
+        this.numeroRetiros = numeroRetiros;
+    }
+
+    public float getTasaAnual() {
+        return tasaAnual;
+    }
+
+    public void setTasaAnual(float tasaAnual) {
+        this.tasaAnual = tasaAnual;
+    }
+
+    public float getComisionMensual() {
+        return comisionMensual;
+    }
+
+    public void setComisionMensual(float comisionMensual) {
+        this.comisionMensual = comisionMensual;
+    }
+    
+}
