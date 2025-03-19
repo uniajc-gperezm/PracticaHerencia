101281759ea8b8f7e5883104900007bdbc46900f
Gabriel Perez <gperezm@profesores.uniajc.edu.co>
2025-03-18 20:47:43 -0500

    Creamos una instancia de la clase cuenta ahorros en la clase principal

--- src/com/uniajc/CuentaCorriente.java
+++ src/com/uniajc/CuentaCorriente.java
@@ -8,6 +8,52 @@ package com.uniajc;
  *
  * @author g.perezmoreno
  */
-public class CuentaCorriente {
+public class CuentaCorriente extends Cuenta {
     
+    protected float sobregiro;
+    
+    public CuentaCorriente(float saldo, float tasaAnual) {
+        super(saldo, tasaAnual);
+        sobregiro = 0;
+    }
+    
+    public void retirar (float cantidad) {
+        float saldoTemporal = saldo - cantidad;
+        
+        if (saldoTemporal < 0) {
+            sobregiro = sobregiro - saldoTemporal;
+            saldo = 0;
+        } else {
+            super.retirar(cantidad);
+        }
+    }
+    
+    public void consignar(float cantidad){
+        // sobregiro = 2000
+        float sobregiroTemporal = sobregiro - cantidad;
+        //sobregiroTemporal = -3000
+        
+        if (sobregiro > 0) {
+            if (sobregiroTemporal > 0) {
+                sobregiro = sobregiroTemporal;
+            }else {
+                saldo = saldo - sobregiroTemporal;
+                sobregiro = 0;
+            }
+        } else {
+            super.consignar(cantidad);
+        }
+    }
+    
+    public void extractoMensual() {
+        super.extractoMensual();
+    }
+    
+    public void imprimir() {
+        System.out.println("saldo: " + saldo);
+        System.out.println("COMISION MENSUAL: " + comisionMensual);
+        System.out.println("Numero DE TRANSACIONES: " + (numeroConsignaciones + numeroRetiros));
+        System.out.println("Sobregiro: " + sobregiro);
+    }
+
 }
--- src/com/uniajc/PracticaHerenciaPoo.java
+++ src/com/uniajc/PracticaHerenciaPoo.java
@@ -15,6 +15,40 @@ public class PracticaHerenciaPoo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("Cuenta de Ahorros: ");
+        
+        final float SALDO = 15000;
+        final float TASA_ANUAL = 0.10f;
+        
+        CuentaAhorros cuentaAhorros1 = new CuentaAhorros(SALDO, TASA_ANUAL);
+        
+        System.out.println("");
+        
+        cuentaAhorros1.imprimir();
+        
+        System.out.println("");
+        
+        cuentaAhorros1.consignar(5000);
+        cuentaAhorros1.consignar(2000);
+        cuentaAhorros1.consignar(3000);
+        cuentaAhorros1.consignar(1000);
+        cuentaAhorros1.imprimir();
+        
+        System.out.println("");
+        
+        System.out.println();
+        // retirar valor de 2000
+        cuentaAhorros1.retirar(2000); // 24000
+        cuentaAhorros1.retirar(1000); // 23000
+        cuentaAhorros1.retirar(100); // 22900
+        cuentaAhorros1.retirar(200); // 22700
+        cuentaAhorros1.retirar(500); // 22200
+        cuentaAhorros1.retirar(500); // 21700
+        cuentaAhorros1.retirar(10000); // 11700
+     
+        cuentaAhorros1.imprimir();
+        
     }
     
 }
